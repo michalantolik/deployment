42736db8a33d577837cf83ad6a76e68830f8f63f
michalantolik <michal.antolik@gmail.com>
2024-12-11 10:32:54 +0100

    Add MVC Core 8.0 demo app

---
+++ Core/MVCDemo-Core-8.0/MVCDemo-Core-8.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCDemo_Core_8._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
